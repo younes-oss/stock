62d62629f837d85cc413b80a92998d5d08c8b7a8
younes-oss <aitchattouyounes@gmail.com>
2025-02-13 11:39:35 +0100

    la conexion avec ma db

--- src/main/java/com/xadmin/produitmanagement/bean/Produit.java
+++ src/main/java/com/xadmin/produitmanagement/bean/Produit.java
@@ -1,5 +1,72 @@
 package com.xadmin.produitmanagement.bean;
 
 public class Produit {
+	
+	private int id;
+	private String nomProduit;
+	private String description;
+	private int quantite , prix ;
+	private String categorie;
+	
+	
+	public Produit(int id, String nomProduit, String description, int quantite, double prix, String categorie) {
+		super();
+		this.id = id;
+		this.nomProduit = nomProduit;
+		this.description = description;
+		this.quantite = quantite;
+		this.prix = (int) prix;
+		this.categorie = categorie;
+	}
+	
+	
+	public Produit(String nomProduit, String description, int quantite, int prix, String categorie) {
+		super();
+		this.nomProduit = nomProduit;
+		this.description = description;
+		this.quantite = quantite;
+		this.prix = prix;
+		this.categorie = categorie;
+	}
 
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNomProduit() {
+		return nomProduit;
+	}
+	public void setNomProduit(String nomProduit) {
+		this.nomProduit = nomProduit;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getQuantite() {
+		return quantite;
+	}
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+	public int getPrix() {
+		return prix;
+	}
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+	public String getCategorie() {
+		return categorie;
+	}
+	public void setCategorie(String categorie) {
+		this.categorie = categorie;
+	}
+	
+	
+	
 }
--- src/main/java/com/xadmin/produitmanagement/dao/ProduitDao.java
+++ src/main/java/com/xadmin/produitmanagement/dao/ProduitDao.java
@@ -1,5 +1,47 @@
 package com.xadmin.produitmanagement.dao;
 
-public class ProduitDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.xadmin.produitmanagement.bean.Produit;
+
+public class ProduitDao {
+	
+	private String jdbcURL = "jdbc:mysql://localhost:3306/product?useSSL=false";
+	private String jdbcUsername = "root";
+	private String jdbcPassword = "admin";
+
+	private static final String INSERT_PRODUCTS_SQL = "INSERT INTO product" + " (name, description, quantite) VALUES "
+			+ " (?, ?, ?);";
+
+	private static final String SELECT_PRODUCT_BY_ID = "select id,name,description,prix,quantite,categorie from product where id =?";
+	private static final String SELECT_ALL_PRODUCTS  = "select * from product";
+	private static final String DELETE_PRODUCTS_SQL  = "delete from product where id = ?;";
+	private static final String UPDATE_PRODUCTS_SQL  = "update product set name = ?,email= ?, country =? where id = ?;";
+	
+	public ProduitDao() {
+		
+	}
+
+	protected Connection getConnection() {
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	
+	
 }
